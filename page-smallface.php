259ddd7f024df86850daa1ad46904f1f35aa96f8
keitayamashita <keepleftman@gmail.com>
2023-07-27 03:48:56 +0900

    page-menuをリネーム

---
+++ page-smallface.php
@@ -0,0 +1,39 @@
+<?php get_header();?>
+<?php custom_breadcrumb(); ?>
+<main class="page_menu">
+<div class="fv">
+    <div class="container w1120">
+        <h1 class="page_ttl"><?php the_title();?></h1>
+    </div>
+</div>
+<article class="art_page">
+    <div class="container">
+    <?php if(have_rows('menu_list_parent')):?>
+        <div class="art_page_contents">
+                <?php while(have_rows('menu_list_parent')): the_row();?>
+                    <div class="menu_list-item">
+                        <h3 class="menu_list_ttl"><?php the_sub_field('menu_list_ttl'); ?></h3>
+                            <div class="data_table">
+                                <div class="row hed">
+                                    <div class="data_table-item name"></div>
+                                    <div class="data_table-item mprice">会員価格</div>
+                                    <div class="data_table-item price">通常価格</div>
+                                </div>
+                                <?php if(have_rows('menu_list_children')):?>
+                                <?php while(have_rows('menu_list_children')): the_row();?>
+                                <div class="row">
+                                    <div class="data_table-item name">¥<?php the_sub_field('product_name'); ?></div>
+                                    <div class="data_table-item mprice">¥<?php the_sub_field('member_price'); ?></div>
+                                    <div class="data_table-item price">¥<?php the_sub_field('default_price'); ?></div>
+                                </div>
+                                <?php endwhile;?>
+                                <?php endif;?>
+                            </div>
+                    </div>
+                <?php endwhile;?>
+        </div>
+    <?php endif;?>
+    </div>
+</article>
+</main>
+<?php get_footer(); ?>
