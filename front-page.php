3135bb3ab9ee4a773f2349ff83b21507241e9b95
keitayamashita <keepleftman@gmail.com>
2023-07-27 05:32:22 +0900

    TOP/メニューコンテンツへリンク付与、ACF繰り返しフィールド設定、不要phpテンプレート削除

--- front-page.php
+++ front-page.php
@@ -158,28 +158,28 @@
         <h2 class="sec_ttl">メニュー</h2>
         <ul class="beautymenu_list">
             <li class="beautymenu_list-item">
-                <a href="">
+                <a href="<?php echo home_url(); ?>/smallface/">
                     <span class="menu_txt">アンチエイジング小顔</span>
                     <img src="<?php echo tmpdir(); ?>/img/top/menu1.jpg" alt="アンチエイジング小顔">
                     <span class="dark-cover"></span>
                 </a>
             </li>
             <li class="beautymenu_list-item">
-                <a href="">
+                <a href="<?php echo home_url(); ?>/aesthetic-dermatology/">
                     <span class="menu_txt">美容皮膚科</span>
                     <img src="<?php echo tmpdir(); ?>/img/top/menu2.jpg" alt="美容皮膚科">
                     <span class="dark-cover"></span>
                 </a>
             </li>
             <li class="beautymenu_list-item">
-                <a href="">
+                <a href="<?php echo home_url(); ?>/hair-removal/">
                     <span class="menu_txt">脱毛</span>
                     <img src="<?php echo tmpdir(); ?>/img/top/menu3.jpg" alt="脱毛">
                     <span class="dark-cover"></span>
                 </a>
             </li>
             <li class="beautymenu_list-item">
-                <a href="">
+                <a href="<?php echo home_url(); ?>/aesthetic-surgery/">
                     <span class="menu_txt">美容外科</span>
                     <img src="<?php echo tmpdir(); ?>/img/top/menu4.jpg" alt="美容外科">
                     <span class="dark-cover"></span>
---
+++ page-hair-removal.php
@@ -0,0 +1,48 @@
+<?php get_header();?>
+<?php custom_breadcrumb(); ?>
+<main class="page_menu">
+<div class="fv">
+    <div class="container w1120">
+        <h1 class="page_ttl"><?php the_title();?></h1>
+    </div>
+</div>
+<article class="art_page">
+    <div class="container">
+    <?php if(have_rows('menu_list_parent')):?>
+        <div class="art_page_contents">
+                <?php while(have_rows('menu_list_parent')): the_row();?>
+                    <div class="menu_list-item">
+                        <h3 class="menu_list_ttl"><?php the_sub_field('menu_list_ttl'); ?></h3>
+                        <?php if(have_rows('menu_list_children')):?>
+                            <?php while(have_rows('menu_list_children')): the_row();?>
+                            <?php if(get_sub_field('list_ttl')):?>
+                            <div class="data_table-item mprice"><?php the_sub_field('list_ttl'); ?></div>
+                            <?php endif;?>
+                            <div class="data_table">
+                                <div class="row hed">
+                                    <div class="data_table-item mprice"><?php the_sub_field('list_sub_ttl1'); ?></div>
+                                    <div class="data_table-item price"><?php the_sub_field('list_sub_ttl2'); ?></div>
+                                </div>
+                                <?php if(have_rows('menu_list_find')):?>
+                                <?php while(have_rows('menu_list_find')): the_row();?>
+                                <div class="row">
+                                    <?php if(get_sub_field('product_ttl')):?>
+                                        <div class="data_table-item mprice"><?php the_sub_field('product_ttl'); ?></div>
+                                    <?php endif;?>
+                                    <?php if(get_sub_field('product_price')):?>
+                                        <div class="data_table-item price">¥<?php the_sub_field('product_price'); ?></div>
+                                    <?php endif;?>
+                                </div>
+                                <?php endwhile;?>
+                                <?php endif;?>
+                            </div>
+                            <?php endwhile;?>
+                        <?php endif;?>
+                    </div>
+                <?php endwhile;?>
+        </div>
+    <?php endif;?>
+    </div>
+</article>
+</main>
+<?php get_footer(); ?>
