8ef123c4ec028ca9d264f820be717011b2a068e0
masaya_oobatake <oooobatake@gmail.com>
2023-07-27 02:20:28 +0900

    改行プラス

--- 404.php
+++ 404.php
@@ -5,6 +5,7 @@
         <h1 class="page_ttl">404 Not Found ページが見つかりません。</h1>
         <p class="art_page-desc">お探しのページが見つかりませんでした。<br>  
     一時的にアクセスできない状態か、移動もしくは削除されてしまった可能性があります。</p>
+    
     </div>
 </article>
 </main>
